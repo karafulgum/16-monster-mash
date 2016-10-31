a896a0e2237f7faf4bfd18809e9c3de29e14c516
Kara Fulgum <karafulgum@gmail.com>
2016-10-31 15:16:01 -0500

    Beginning project

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+
+})
+</script>
--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
 import 'whatwg-fetch';
+
+import Vue from './app.vue';
+
+new App({
+  el: '.app',
+});
