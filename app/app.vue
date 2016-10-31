2293208d7cbe065c66ef866d6f107931a3ac9aa5
Kara Fulgum <karafulgum@gmail.com>
2016-10-31 17:17:26 -0500

    Sidebar styling

--- app/app.vue
+++ app/app.vue
@@ -1,32 +1,35 @@
 <template lang="html">
   <div class="app">
-    <div class="left-side">
-      <p class="left-side__title">Build Your Monster</p>
-      <div class="left-side__img-switcher">
-        <button class="left-side__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
-        <div class="frame">
-          <img src="/monster/body-1.png" alt=""/>
+    <div class="sidebar">
+      <p class="sidebar__title">Build Your Monster</p>
+      <div class="sidebar__img-switcher">
+        <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+        <div class="sidebar__frame">
+          <img class="sidebar__img" src="/monster/body-1.png" alt=""/>
         </div>
-        <button class="left-side__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+        <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
-      <div class="left-side__img-switcher">
-        <button class="left-side__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
-        <div class="frame">
-          <img src="/monster/eyes-1-crossed.png" alt=""/>
+      <div class="sidebar__img-switcher">
+        <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+        <div class="sidebar__frame">
+          <img class="sidebar__img" src="/monster/mouth-1.png" alt=""/>
         </div>
-        <button class="left-side__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+        <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
-      <div class="left-side__img-switcher">
-        <button class="left-side__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
-        <div class="frame">
-          <img src="/monster/mouth-1.png" alt=""/>
+      <div class="sidebar__img-switcher">
+        <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+        <div class="sidebar__frame">
+          <img class="sidebar__img" src="/monster/eyes-1-crossed.png" alt=""/>
         </div>
-        <button class="left-side__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+        <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
-      <form>
-        <input type="text" name="name" placeholder="Name Your Creation">
-        <button class="form__submit">Save Favorite</button>
+      <form class="sidebar__form">
+        <input class="sidebar__input" type="text" name="name" placeholder="Name Your Creation">
+        <button class="sidebar__submit">Save Favorite</button>
       </form>
+    </div>
+    <div class="app__main">
+
     </div>
   </div>
 </template>
@@ -37,7 +40,7 @@ import Vue from 'vue';
 export default Vue.extend({
   data() {
     return {
-      monsters: [],
+      monsterParts: [],
       form: {
         name: 'monster-name'
       },
