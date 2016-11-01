f57b5f7e25674e42aacbffef02f9a7db003e5f24
Kara Fulgum <karafulgum@gmail.com>
2016-10-31 21:01:22 -0500

    Main and favorites HTML

--- app/app.vue
+++ app/app.vue
@@ -1,25 +1,26 @@
 <template lang="html">
   <div class="app">
+
     <div class="sidebar">
       <p class="sidebar__title">Build Your Monster</p>
       <div class="sidebar__img-switcher">
         <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
         <div class="sidebar__frame">
-          <img class="sidebar__img" src="/monster/body-1.png" alt=""/>
+          <img class="sidebar__img" src="/monster/body-1.png" alt="" />
         </div>
         <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
       <div class="sidebar__img-switcher">
         <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
         <div class="sidebar__frame">
-          <img class="sidebar__img" src="/monster/mouth-1.png" alt=""/>
+          <img class="sidebar__img" src="/monster/mouth-1.png" alt="" />
         </div>
         <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
       <div class="sidebar__img-switcher">
         <button class="sidebar__btn"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
         <div class="sidebar__frame">
-          <img class="sidebar__img" src="/monster/eyes-1-crossed.png" alt=""/>
+          <img class="sidebar__img" src="/monster/eyes-1-crossed.png" alt="" />
         </div>
         <button class="sidebar__btn"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
       </div>
@@ -28,8 +29,34 @@
         <button class="sidebar__submit">Save Favorite</button>
       </form>
     </div>
-    <div class="app__main">
 
+    <div class="app__content">
+      <div class="main">
+        <img src="/monster/body-1.full.png" alt="" class="main__img">
+        <!-- <img src="/monster/eyes-1-crossed.full.png" alt="" class="content__img">
+        <img src="/monster/mouth-1.full.png" alt="" class="content__img"> -->
+      </div>
+
+      <div class="favorites">
+        <div class="favorites__group">
+          <div class="favorites__frame">
+            <img src="/monster/body-1.full.png" alt="" class="favorites__img">
+          </div>
+          <p class="favorites__name">Kara</p>
+        </div>
+        <div class="favorites__group">
+          <div class="favorites__frame">
+            <img src="/monster/body-1.full.png" alt="" class="favorites__img">
+          </div>
+          <p class="favorites__name">Cooper</p>
+        </div>
+        <div class="favorites__group">
+          <div class="favorites__frame">
+            <img src="/monster/body-1.full.png" alt="" class="favorites__img">
+          </div>
+          <p class="favorites__name">Maggie</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,13 +65,13 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-  data() {
-    return {
-      monsterParts: [],
-      form: {
-        name: 'monster-name'
-      },
-    };
-  },
+    data() {
+        return {
+            monsterParts: [],
+            form: {
+                name: 'monster-name'
+            },
+        };
+    },
 });
 </script>
